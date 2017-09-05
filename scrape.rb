b8e804c53c33aabc09c83cbbe0567bd1275681a7
conorbroderick <cjayjayb@gmail.com>
2017-09-05 15:19:11 +0100

    Refactoring

--- scrape.rb
+++ scrape.rb
@@ -1,74 +1,85 @@
 require 'mechanize'
-require 'pry'
 require_relative 'secrets'
 
-# Create new agent
-mech = Mechanize.new
+# Login and return admin homepage.
+def login
+  mech = Mechanize.new
+  page = mech.get(LOGIN_URL)
+  username_field = page.form.field_with(id: 'user_email')
+  username_field.value = USER
+  password_field = page.form.field_with(id: 'user_password')
+  password_field.value = PASS
+  page.form.submit
+end
 
-# Init File I/O
-file = open('results.txt', 'w')
+# Return links of all courses.
+def getCourseLinks(page)
+  page.links.each { |link|
+    if link.href == "/courses"
+      return link.click.links
+    end
+  }
+end
 
-# Login
-page = mech.get(LOGIN_URL)
-username_field = page.form.field_with(id: 'user_email')
-username_field.value = USER
-password_field = page.form.field_with(id: 'user_password')
-password_field.value = PASS
-page = page.form.submit
-
-# Nav to all courses page
-page.links.each { |link|
-  if link.href == "/courses"
-	page = link.click
-	break
+# Scrape all courses.
+def scrapeCourses(course_links)
+  # Init File I/O
+  file = open('results.txt', 'w')
+  # Loop through all courses.
+  for course_link in course_links
+    # Hack to differentiate course links from other links on the page.
+    next if course_link.text.length < 100 || course_link.text.split(" ").join(" ") == "Prometheus Fundamentals"
+    course_title = course_link.text.split(" ").join(" ")
+    writeCourseTitle(file, course_title)
+    puts "Getting next course.....#{course_title}"
+    scrapeLectures(file, course_link.click.links)
   end
-}
+  file.close
+end
 
-course_links = page.links
-# Loop through all courses
-for course_link in course_links
-  # Hack to differentiate course links from other links on the page
-  if course_link.text.length < 100 || course_link.text.split(" ").join(" ") == "Prometheus Fundamentals"
-    next
+# Scrape all lectures of given course.
+def scrapeLectures(file, lecture_links)
+  # Loop through all lectures in given course.
+  for lecture_link in lecture_links
+    # Hack to differentiate lecture links from other links on the page.
+    next if lecture_link.text.length < 100
+    page = lecture_link.click
+
+    # Get lecture title.
+    title = getLectureTitle(page)
+    puts "Scraping next lecture........#{title}"
+
+    # Hack to skip over quizes.
+    next if page.at("div.lecture-text-container").nil?
+
+    # Get lecture content.
+    content = ""
+    page.at("div.lecture-text-container").children.children.each do |p|
+      content <<  p
+    end
+    writeLectureContent(file, title, content)
   end
-  course_title = course_link.text.split(" ").join(" ")
-  puts "Getting next course.....#{course_title}"
+end
+
+def getLectureTitle(page)
+  page.at("h2#lecture_heading.section-title").children.last.text.split(" ").join(" ")
+end
+
+def writeCourseTitle(file, course_title)
   file.write("*******************************\n")
   file.write("///////////////////////////////\n")
   file.write(course_title + "\n")
   file.write("///////////////////////////////\n")
   file.write("*******************************\n\n")
-  page = course_link.click
-  lecture_links = page.links
-
-  # Loop through all lectures in given course
-  for lecture_link in lecture_links
-    # Hack to differentiate lecture links from other links on the page
-    if lecture_link.text.length < 100
-      next
-    end
-    page = lecture_link.click
-
-    # Get lecture title
-    title = page.at("h2#lecture_heading.section-title").children.last.text.split(" ").join(" ")
-
-    # Get lecture content
-    content = ""
-    unless page.at("div.lecture-text-container").nil?
-      page.at("div.lecture-text-container").children.children.each do |p|
-        content <<  p
-      end
-
-      puts "Scraping next lecture........#{title}"
-
-      # Write scraped results to file
-      file.write("/////\n")
-      file.write(title + "\n")
-      file.write("/////\n\n")
-      file.write(content + "\n")
-    end
-  end
 end
 
-file.close
+def writeLectureContent(file, title, content)
+  # Write scraped results to file.
+  file.write("/////\n")
+  file.write(title + "\n")
+  file.write("/////\n\n")
+  file.write(content + "\n")
+end
+
+scrapeCourses(getCourseLinks(login))
 puts "Finished! Check results.txt for all courses and their text content :)"
